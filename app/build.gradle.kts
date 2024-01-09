b4d0a64cf422823aa903f0e47f607057993cf627
ChoiUS <60271512+choius323@users.noreply.github.com>
2024-01-09 16:57:15 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,5 +60,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
     androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
-    androidTestImplementation('androidx.test:runner:1.4.0')
+    androidTestImplementation("androidx.test:runner:1.5.2")
 }
